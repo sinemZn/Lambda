8a978eb13b09c9550e1077911d8fcde56efbb5fc
Sinem Z <94461108+sinemZn@users.noreply.github.com>
2022-02-23 19:13:59 +0000

    --

---
+++ _05_StringManipulation/_05_StringManipulation/Q01.java
@@ -0,0 +1,14 @@
+package _05_StringManipulation;
+
+import java.util.Scanner;
+
+public class Q01 {
+    public static void main(String[] args) {
+        // Kullanıcıdan alacağını bir stringde boşluk karakterinin olup olmadığını kontrol ediniz.
+        
+
+        // Kullanıcıdan alacağını bir stringin bos olup olmadığını kontrol ediniz.
+       
+    }
+}
+
---
+++ _05_StringManipulation/_05_StringManipulation/Q03.java
@@ -0,0 +1,11 @@
+package _05_StringManipulation;
+
+import java.util.Scanner;
+
+public class Q03 {
+    public static void main(String[] args) {
+        //Kullanıcıdan alacağınız, 3 kelimeden oluşan ismi
+        // Örn. Ahmet Emin Yılmaz -> A.E.Y.  formatıyla yazdırınız.
+
+    }       
+}
